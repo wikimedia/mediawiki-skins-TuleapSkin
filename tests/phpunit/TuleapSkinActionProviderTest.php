687ad775a873e28671310bba634220b5fa30de55
miriam schlindwein <schlindwein@hallowelt.com>
2022-07-25 14:36:11 +0200

    Fix skin feedback
    Change-Id: Id06fe60efd1982802fc383c17e3f044fcc12a316

---
+++ tests/phpunit/TuleapSkinActionProviderTest.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace TuleapSkin\Test;
+
+use MediaWikiIntegrationTestCase;
+use TuleapSkin\TuleapSkinActionProvider;
+
+class TuleapSkinActionProviderTest extends MediaWikiIntegrationTestCase {
+
+	/**
+	 *
+	 * @return void
+	 * @dataProvider provideGetLinksTestData
+	 * @covers TuleapSkin\TuleapSkinActionProvider::getLinks
+	 */
+	public function testGetLinks( $configLinks, $allLinks, $expectedLinks ) {
+		$provider = new TuleapSkinActionProvider( $allLinks );
+		$links = $provider->getLinks( $configLinks );
+
+		$this->assertArrayEquals( $expectedLinks, $links );
+	}
+
+	public function provideGetLinksTestData() {
+		$links = [
+			'main' => [
+				"class" => "selected",
+				"text" => "Main page",
+				"id" => "ca-nstab-main"
+			],
+			'talk' => [
+				"text" => "Discussion",
+				"id" => "ca-talk"
+			],
+			'history' => [
+				"text" => "History",
+				"id" => "ca-history"
+			],
+			'info' => [
+				"text" => "Page information",
+				"id" => "t-info"
+			],
+			'view' => [
+				"text" => "View",
+				"id" => "ca-view"
+			],
+			'edit' => [
+				"text" => "Edit",
+				"id" => "ca-edit"
+			],
+			'pdfbook' => [
+				"text" => "Print as PDF",
+				"id" => "ca-pdfbook"
+			],
+			'delete' => [
+				"text" => "Delete",
+				"id" => "ca-delete"
+			],
+			'move' => [
+				"text" => "Move",
+				"id" => "ca-move"
+			],
+			'protect' => [
+				"text" => "Protect",
+				"id" => "ca-protect"
+			],
+			'watch' => [
+				"text" => "Watch",
+				"id" => "ca-watch"
+			],
+			'whatlinkshere' => [
+				"text" => "Links",
+				"id" => "t-whatlinkshere"
+			],
+			'recentchangeslinked' => [
+				"text" => "Linked",
+				"id" => "t-recentchangeslinked"
+			],
+			'specialpages' => [
+				"text" => "Printable version",
+				"id" => "t-print"
+			],
+			'permalink' => [
+				"text" => "Permanent link",
+				"id" => "t-permalink"
+			]
+		];
+		$shortenedlinks = [
+			'recentchangeslinked' => [
+				"text" => "Linked",
+				"id" => "t-recentchangeslinked"
+			],
+			'specialpages' => [
+				"text" => "Printable version",
+				"id" => "t-print"
+			]
+		];
+		return [
+			'actions-with-starting-dash' => [
+				[
+					"-", "whatlinkshere", "-", "protect"
+				],
+				$links,
+				[
+					'whatlinkshere' => [
+						"text" => "Links",
+						"id" => "t-whatlinkshere"
+					],
+					'separator-1' => 'separator',
+					'protect' => [
+						"text" => "Protect",
+						"id" => "ca-protect"
+					]
+				]
+			],
+			'actions-empty' => [
+				[],
+				$links,
+				[]
+			],
+			'multiple-actions-with-star' => [
+				[
+					"recentchangeslinked", "-", "upload", "specialpages", "-", "*"
+				],
+				$links,
+				[
+					'recentchangeslinked' => [
+						"text" => "Linked",
+						"id" => "t-recentchangeslinked"
+					],
+					'separator-1' => 'separator',
+					'specialpages' => [
+						"text" => "Printable version",
+						"id" => "t-print"
+					],
+					'separator-3' => 'separator',
+					'talk' => [
+						"text" => "Discussion",
+						"id" => "ca-talk"
+					],
+					'history' => [
+						"text" => "History",
+						"id" => "ca-history"
+					],
+					'info' => [
+						"text" => "Page information",
+						"id" => "t-info"
+					],
+					'pdfbook' => [
+						"text" => "Print as PDF",
+						"id" => "ca-pdfbook"
+					],
+					'delete' => [
+						"text" => "Delete",
+						"id" => "ca-delete"
+					],
+					'move' => [
+						"text" => "Move",
+						"id" => "ca-move"
+					],
+					'protect' => [
+						"text" => "Protect",
+						"id" => "ca-protect"
+					],
+					'watch' => [
+						"text" => "Watch",
+						"id" => "ca-watch"
+					],
+					'whatlinkshere' => [
+						"text" => "Links",
+						"id" => "t-whatlinkshere"
+					],
+					'permalink' => [
+						"text" => "Permanent link",
+						"id" => "t-permalink"
+					]
+				]
+			],
+			'multiple-actions-with-star-at-end' => [
+				[
+					"recentchangeslinked", "-", "upload", "specialpages", "-", "*"
+				],
+				$shortenedlinks,
+				[
+					'recentchangeslinked' => [
+						"text" => "Linked",
+						"id" => "t-recentchangeslinked"
+					],
+					'separator-1' => 'separator',
+					'specialpages' => [
+						"text" => "Printable version",
+						"id" => "t-print"
+					]
+				]
+			],
+			'multiple-actions-with-star-middle' => [
+				[
+					"recentchangeslinked", "-", "*", "-", "upload", "specialpages", "-", "*"
+				],
+				$shortenedlinks,
+				[
+					'recentchangeslinked' => [
+						"text" => "Linked",
+						"id" => "t-recentchangeslinked"
+					],
+					'separator-1' => 'separator',
+					'specialpages' => [
+						"text" => "Printable version",
+						"id" => "t-print"
+					]
+				]
+			]
+		];
+	}
+
+}
