f0e77175bd2da0f2bf09aa90df79d1312dbae336
miriam schlindwein <schlindwein@hallowelt.com>
2022-11-23 15:26:49 +0100

    Fix eslint tests
    Change-Id: I82c1b7863a0abd1244ac06f23df76296ff99e002

--- resources/js/Dropdown.js
+++ resources/js/Dropdown.js
@@ -1,23 +1,30 @@
 ( function ( mw, $ ) {
-	$( document ).click( function ( e ) {
+	$( document ).on( 'click', function ( e ) {
 		var $dropdown, $parent,
-			$dropdownMenu = $( '.mw-tlp-skin-btn-dropdown-menu' );
-		if ( $( e.target ).hasClass( 'mw-tlp-dropdown-btn' ) ) {
-			$dropdown = $( e.target.nextElementSibling );
+			// eslint-disable-next-line no-jquery/no-global-selector
+			$dropdownMenu = $( '.mw-tlp-skin-btn-dropdown-menu' ),
+			target = e.target;
+
+		// eslint-disable-next-line no-jquery/no-class-state
+		if ( $( target ).hasClass( 'mw-tlp-dropdown-btn' ) ) {
+			$dropdown = $( target.nextElementSibling );
 			if ( $dropdown.length < 1 ) {
-				$parent = $( e.target.offsetParent );
+				$parent = target.offsetParent;
 				$dropdown = $( $parent ).children().last();
 			}
 
+			// eslint-disable-next-line no-jquery/no-class-state
 			if ( $dropdown.hasClass( 'dropdown-shown' ) ) {
 				$dropdown.removeClass( 'dropdown-shown' );
 			} else {
+				// eslint-disable-next-line no-jquery/no-class-state
 				if ( $dropdownMenu.hasClass( 'dropdown-shown' ) ) {
 					$dropdownMenu.removeClass( 'dropdown-shown' );
 				}
 				$dropdown.addClass( 'dropdown-shown' );
 			}
 		} else {
+			// eslint-disable-next-line no-jquery/no-class-state
 			if ( $dropdownMenu.hasClass( 'dropdown-shown' ) ) {
 				$dropdownMenu.removeClass( 'dropdown-shown' );
 			}
--- resources/js/VE-Toolbar.js
+++ resources/js/VE-Toolbar.js
@@ -1,12 +1,18 @@
 ( function ( mw, $ ) {
-	$( window ).scroll( function () {
+	var $visualFloatingVE, $sourceFloatingVE;
+	$( window ).on( 'scroll', function () {
+		// eslint-disable-next-line no-jquery/no-global-selector
 		if ( $( '.ve-init-target-visual' ).length || $( '.ve-init-target-source' ).length ) {
-			var $visualFloatingVE = $( '.ve-init-target-visual > .ve-ui-toolbar-floating' ),
-				$sourceFloatingVE = $( '.ve-init-target-source > .ve-ui-toolbar-floating' );
+			// eslint-disable-next-line no-jquery/no-global-selector
+			$visualFloatingVE = $( '.ve-init-target-visual > .ve-ui-toolbar-floating' );
+			// eslint-disable-next-line no-jquery/no-global-selector
+			$sourceFloatingVE = $( '.ve-init-target-source > .ve-ui-toolbar-floating' );
 
 			if ( $visualFloatingVE.length || $sourceFloatingVE.length ) {
+				// eslint-disable-next-line no-jquery/no-global-selector
 				$( '.fixed-container' ).addClass( 've-fixed-btn' );
 			} else {
+				// eslint-disable-next-line no-jquery/no-global-selector
 				$( '.fixed-container' ).removeClass( 've-fixed-btn' );
 			}
 		}
