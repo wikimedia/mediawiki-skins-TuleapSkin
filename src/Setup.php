abd8759537a8e78c68347ea9772fca4d6d561c08
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-11-14 14:45:43 +0100

    fix: Remove call to non existing method
    request #45620 MediaWiki upgrade from 1.39 to 1.43
    
    Change-Id: I1fab7bcf4bf5e6153697525a636ce5df8771d16e

--- src/Setup.php
+++ src/Setup.php
@@ -9,9 +9,6 @@ class Setup {
 	 * @return void
 	 */
 	public static function onCallback() {
-		// @phan-suppress-next-line PhanUndeclaredFunction
-		mwsInitComponents();
-
 		$GLOBALS['wgVisualEditorSupportedSkins'][] = 'tuleap';
 		$GLOBALS['wgVisualEditorSkinToolbarScrollOffset']['tuleap'] = 100;
 	}
